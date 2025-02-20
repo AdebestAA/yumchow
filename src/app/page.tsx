5b99e5998215597544c116272f83ace487bf6aba
AdebestAA <aadebesta@gmail.com>
2025-02-20 17:24:55 +0100

    now done

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,15 +4,15 @@ import Link from "next/link";
 
 export default function Home() {
   return (
-    <>
-    <Loader/>
-    </>
-  //   <div className="">
-  // <h1 className="">player</h1>
-  // <img src="yumChow.png"/>
-  // <Link href={"/signup"}>Sign up</Link>
-  // <br />
-  // <Link href={"/signin"}>Sign in</Link>
-  //   </div>
+    // <>
+    // <Loader/>
+    // </>
+    <div className="">
+  <h1 className="">player</h1>
+  <img src="yumChow.png"/>
+  <Link href={"/signup"}>Sign up</Link>
+  <br />
+  <Link href={"/signin"}>Sign in</Link>
+    </div>
   );
 }
